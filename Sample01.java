435b3720249c66f14ddc520bdeba5778e15f28fd
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-11-06 16:39:04 +0530

    Update Sample01.java

--- Sample01.java
+++ Sample01.java
@@ -15,3 +15,11 @@ public static void main(String giri[]){
 }
 }
 }
+class Sample04{
+public static void main(String giri[]){
+ int i = 5;
+ for(i=5;i<=10;i++){
+     System.out.println("Class Name Sample01");
+}
+}
+}
