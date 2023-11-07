d29f1c818abe994181ac4da105b708efae244645
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-11-07 15:37:58 +0530

    Update Sample01.java

--- Sample01.java
+++ Sample01.java
@@ -2,7 +2,7 @@ class Demo{
 public static void main(String giri[]){
  int i = 5;
  for(i=5;i<=10;i++){
-     System.out.println("Class Name Demo");
+     System.out.println("Thank You!!!");
 }
 }
 }
